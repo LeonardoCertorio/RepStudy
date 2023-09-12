621890ba4ef25bf70ab65527c1e65c739bd4c053
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-09-12 17:36:11 -0300

    Update, ex B4

--- Dart/dart_application_1/lib/dart_application_1.dart
+++ Dart/dart_application_1/lib/dart_application_1.dart
@@ -138,3 +138,15 @@ void mapaIdadeNomes() {
 }
 
 //Ex B4
+String intersec() {
+  List lista1 = [5,3,6,7,7,2,9];
+  List lista2 = [0,1,2,2,3,4,5];
+  List encontro = [];
+	
+	for (int i in lista1) {
+    if (lista2.contains(i)) {
+      encontro.add(i);
+    }    
+  }
+  return "A intersecção entre os conjuntos é $encontro";
+}
