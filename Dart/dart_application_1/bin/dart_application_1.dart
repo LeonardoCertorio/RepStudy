94aaa77aba038616464dcf8e23bcb2cc1d476136
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-09-12 21:49:08 -0300

    Update

--- Dart/dart_application_1/bin/dart_application_1.dart
+++ Dart/dart_application_1/bin/dart_application_1.dart
@@ -1,13 +1,13 @@
 import 'package:dart_application_1/dart_application_1.dart' as dart_application_1;
 
 void main(List<String> arguments) {
-  //print('Hello world: ${dart_application_1.calculate()}!');
+  //print('Hello world: ${dart_application_1.olaMundo()}!');
   //print(dart_application_1.oi());  
   //print(dart_application_1.compara());
   //print(dart_application_1.areaCirculo());
   //print(dart_application_1.nomeIdade());
   //print(dart_application_1.numerosPares()); 
   //print(dart_application_1.funcBubbleSort()); 
-  //print(dart_application_1.mapaIdadeNomes());
+  //dart_application_1.mapaIdadeNomes();
   //print(dart_application_1.intersec());
 }
