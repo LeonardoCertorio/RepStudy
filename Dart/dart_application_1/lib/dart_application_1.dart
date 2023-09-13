4eeef218121cbbb2b7cf7984cd1361fb88216025
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-09-12 21:48:27 -0300

    Update exs

--- Dart/dart_application_1/lib/dart_application_1.dart
+++ Dart/dart_application_1/lib/dart_application_1.dart
@@ -88,21 +88,22 @@ String nomeIdade() {
 //Ex B1
 String numerosPares() {
   print("Gostaria de quantos números pares em sequência?");
-  int tam = stdin.readLineSync();
-  if (tam.isEmpty) {
+  String resposta = stdin.readLineSync() ?? "";
+  if (resposta.isEmpty) {
     return 'Digite um número válido';
   } else {  
-  List n = [];
-  n.length = tam;
-  int i=0; 
-  while(n.length < tam){
-    if(i%2==0){
-      n.add(i);
+    int tam = int.parse(resposta);
+    List n = [];
+    n.length = tam;
+    int i=0; 
+    while(n.length < tam){
+      if(i%2==0){
+        n.add(i);
+      }
+      i++;
     }
-    i++;
+    return 'Estes são os números pares: $n';
   }
-  }
-  return 'Estes são os números pares: $n';
 }
 
 //Ex B2
