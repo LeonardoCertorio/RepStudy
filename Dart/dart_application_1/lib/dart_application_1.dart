713fe67bcc45ac477596e8d421a5877aa0c1b038
Leonardo Certorio <118929833+LeonardoCertorio@users.noreply.github.com>
2023-09-13 14:22:36 -0300

    Update, ex B5

--- Dart/dart_application_1/lib/dart_application_1.dart
+++ Dart/dart_application_1/lib/dart_application_1.dart
@@ -153,3 +153,28 @@ void intersec() {
 }
 
 //Ex B5
+void exclusivos() {
+  List lista1 = [5,3,6,7,7,2,9];
+  List lista2 = [0,1,2,2,3,4,5];
+  List soma = lista1 + lista2;
+
+  List encontro = [];
+  List exclusivos = [];
+
+  //Define a intersecção das listas
+  for (int i in lista1) {
+    if (lista2.contains(i)) {
+      encontro.add(i);
+    }    
+  }
+
+  //Define os exclusivos, subtraindo a união das listas pela intersecção
+  for (var j in soma) {
+    if (!encontro.contains(j)) {
+      exclusivos.add(j);
+    }
+  }
+  print("Elementos únicos das listas: $exclusivos");
+}
+
+//C1
